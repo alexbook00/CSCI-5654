edbf564c4b56d72404fedfae643a2306784f3daf
alexbook00 <43623251+alexbook00@users.noreply.github.com>
2022-02-25 16:58:10 -0700

    Add files via upload

--- Homework/HW3/assignment3_problem4.py
+++ Homework/HW3/assignment3_problem4.py
@@ -1,10 +1,10 @@
 import numpy as np
 from pprint import pprint
 
-a = np.array([
+A = np.array([
 	[0, -2, 1],
 	[-2, 0, 1],
-	[0, -2, 1],
+	[0, -2, -1],
 	[-2, 0, -1],
 	[2, 0, -1],
 	[2, 0, 1],
@@ -16,32 +16,22 @@ b = np.array([
 	0, 0, 0, 0, 4, 4, 4, 4
 ])
 
-solution_dict = {}
-
-for i in range(len(a)):
-	for j in range(len(a)):
-		for k in range(len(a)):
+for i in range(len(A)):
+	for j in range(len(A)):
+		for k in range(len(A)):
 			lst = [i, j, k]
 			lst.sort()
 			curr_combo = tuple(lst)
-			if i != j and i != k and j != k and curr_combo not in solution_dict.keys():
-				new_a = np.array([a[i], a[j], a[k]])
-				new_b = np.array([b[i], b[j], b[k]])
+			if i != j and i != k and j != k:
+				new_A = A[[i, j, k]]
+				new_b = b[[i, j, k]]
 				# solve the system to get the solution
 				try:
-					x = np.linalg.solve(new_a, new_b)
-					solution_dict[curr_combo] = x
+					candidate_vertex = np.linalg.solve(new_A, new_b)
 				except:
-					solution_dict[curr_combo] = 0
+					continue
 
-saturated_constraints_dict = {}
-for key, value in solution_dict.items():
-	if not isinstance(value, int):
-		for i in range(len(a)):
-			if np.allclose(np.dot(a[i], value), b[i]):
-				if key not in saturated_constraints_dict.keys():
-					saturated_constraints_dict[key] = [f'C_{i+1}']
-				else:
-					saturated_constraints_dict[key].append(f'C_{i+1}')
-
-print(saturated_constraints_dict)
+				# check if candidate_solution satisfies all 8 constraints
+				constraints_saturated = np.where(np.isclose(A @ candidate_vertex, b))[0] + 1
+				if np.all(A @ candidate_vertex <= b) and len(constraints_saturated) > 2:
+					print(f'########## \n Vertex: {candidate_vertex} \n Constraints Saturated: {constraints_saturated}')
